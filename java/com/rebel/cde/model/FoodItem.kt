d2e02e74eeb01b6877b3ad99f626d6e8f5999658
SURAJ ANAND <47687977+rebelsuraj1506@users.noreply.github.com>
2024-07-03 22:47:11 +0530

    Only  res left(app/src/main/res)

---
+++ java/com/rebel/cde/model/FoodItem.kt
@@ -0,0 +1,3 @@
+package com.rebel.cde.model
+
+data class FoodItem(val id: String?, val name: String?, val cost: Int?)
---
+++ java/com/rebel/cde/util/DrawerLocker.kt
@@ -0,0 +1,5 @@
+package com.rebel.cde.util
+
+interface DrawerLocker {
+    fun setDrawerEnabled(enabled: Boolean)
+}
